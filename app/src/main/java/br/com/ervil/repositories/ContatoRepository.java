37978cdeb79c8e0596302034a41270009ccc56c3
Rodrigo Kumabe <rodrigo.kumabe@gmail.com>
2022-07-22 15:05:11 -0300

    cleanup

--- app/src/main/java/br/com/ervil/repositories/ContatoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.ervil.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import br.com.ervil.models.Contato;
-
-@Repository
-public interface ContatoRepository extends JpaRepository<Contato, Long>{}
