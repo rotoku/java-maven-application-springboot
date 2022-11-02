f893240f57f984a85febe30d0b1381607540e71e
Rodrigo Kumabe <rodrigo.kumabe@gmail.com>
2022-11-02 08:09:51 -0300

    add: application.yml to test

---
+++ app/src/test/java/br/com/ervil/SpringAppTests.java
@@ -0,0 +1,10 @@
+package br.com.ervil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringAppTests {
+    @Test
+    public void contextLoads(){}
+}
